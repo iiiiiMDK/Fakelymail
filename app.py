95d5bc929fc8c9e33aa3548e401a747ba5fe6580
iNDK <s3oooody500000@gmail.com>
2025-04-25 14:41:39 +0300

    Add Google verification file route

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def google_verify():
     return send_from_directory('static', 'googleddb8bfe162a125fe.html')
 
 if __name__ == '__main__':
-       app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+ app.run(debug=True)
